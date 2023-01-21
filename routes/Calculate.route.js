3346d605c4ddc8c94da59df108151b891bdfb81b
arjunsen <arjunsen717@gmail.com>
2023-01-21 15:16:48 +0530

    update

--- routes/Calculate.route.js
+++ routes/Calculate.route.js
@@ -4,15 +4,14 @@ const { CalculateModel } = require('../models/Calculate.model');
 const { body, validationResult } = require('express-validator');
 const {VerifyToken} = require('../Middleware/VerifyToken')
 
-calculateRouter.post("/calculate", VerifyToken, async (req, res) =>{
+calculateRouter.post("/calculate", async (req, res) =>{
    try {
-    let _id = req.authId;
     let {annualInstalmentAmount, annualInterestRate, totalNumberofYears} = req.body
     let investmentAmount = annualInstalmentAmount * totalNumberofYears;
     let rate = annualInterestRate / 100;
     let totalMaturityValue = investmentAmount * (Math.pow((1 + (rate / 1)), (1 * totalNumberofYears)));
     let interest = totalMaturityValue - investmentAmount;
-    res.send({investmentAmount, totalMaturityValue, interest})
+    res.send({investmentAmount, totalMaturityValue : Math.floor(totalMaturityValue), interest : Math.floor(interest)})
    } catch (error) {
     res.status(400).send({msg : "Somthing Went Wrong", error})
    }
